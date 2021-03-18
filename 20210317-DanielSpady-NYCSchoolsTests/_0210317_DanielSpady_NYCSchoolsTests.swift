462f811eb7469c322726f75654819dcb8c0a6576
Daniel Mac Mini <danielspady@gmail.com>
2021-03-17 21:13:31 -0400

    Add Support for Swiftlint
    Add rules to enforce Swiftlint Coding Style
    Auto Correct initial Project Files
    Make changes to Names to enforce Swiftlint styling

--- 20210317-DanielSpady-NYCSchoolsTests/_0210317_DanielSpady_NYCSchoolsTests.swift
+++ 20210317-DanielSpady-NYCSchoolsTests/_0210317_DanielSpady_NYCSchoolsTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import _0210317_DanielSpady_NYCSchools
 
-class _0210317_DanielSpady_NYCSchoolsTests: XCTestCase {
+class NYCSchoolsTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
