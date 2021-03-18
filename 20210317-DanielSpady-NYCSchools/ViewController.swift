72bb8dd8f0cd1ac859546ef6c16373b6d3fd0487
Daniel Mac Mini <danielspady@gmail.com>
2021-03-17 20:49:15 -0400

    Initial Commit

---
+++ 20210317-DanielSpady-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210317-DanielSpady-NYCSchools
+//
+//  Created by Daniel Spady on 2021-03-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 20210317-DanielSpady-NYCSchoolsTests/_0210317_DanielSpady_NYCSchoolsTests.swift
@@ -0,0 +1,33 @@
+//
+//  _0210317_DanielSpady_NYCSchoolsTests.swift
+//  20210317-DanielSpady-NYCSchoolsTests
+//
+//  Created by Daniel Spady on 2021-03-17.
+//
+
+import XCTest
+@testable import _0210317_DanielSpady_NYCSchools
+
+class _0210317_DanielSpady_NYCSchoolsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
